3ad6fd6a94f3122fc2ff85ee8f91b17094219129
illi-homz <gopkabel@mailru>
2021-05-27 19:14:16 +0300

    change Photo model

--- app/models/Photo.py
+++ app/models/Photo.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Photo(models.Model):
-    title: models.CharField(max_length=50, default='')
+    title = models.CharField(max_length=50, default='')
     img = models.ImageField(upload_to='galery', verbose_name='Фото')
 
     def __str__(self):
