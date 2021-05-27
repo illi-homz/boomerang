d0b07f42ed06e24126daf7345e6106cd7cad169d
Ilya <54039668+illi-homz@users.noreply.github.com>
2021-05-27 16:40:33 +0300

    set photo model

--- app/models/Photo.py
+++ app/models/Photo.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Photo(models.Model):
-    img = models.ImageField(upload_to='galery/', verbose_name='Фото')
+    img = models.ImageField(upload_to='galery', verbose_name='Фото')
