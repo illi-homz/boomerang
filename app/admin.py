e2963af869c32fd8ceabdc4963791d5a35b9d403
illi-homz <gopkabel@mail.ru>
2021-08-13 08:28:36 +0300

    new theme

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.New)
-admin.site.register(models.Service)
-admin.site.register(models.Stock)
+# admin.site.register(models.New)
+# admin.site.register(models.Service)
+# admin.site.register(models.Stock)
 admin.site.register(models.Article)
 admin.site.register(models.Document)
 admin.site.register(models.Photo)
 admin.site.register(models.Client)
 admin.site.register(models.Feedback)
 admin.site.register(models.Link)
+
+
+def dublicate_ad(modeladmin, request, queryset):
+	#клонирование выбранных Ad
+	for el in queryset:
+		el.pk = None
+		el.save()
+
+dublicate_ad.short_description = "Дублировать объект"
+
+@admin.register(models.Service)
+class ServiceAdmin(admin.ModelAdmin):
+	actions = [dublicate_ad]
+
+@admin.register(models.New)
+class NewAdmin(admin.ModelAdmin):
+	actions = [dublicate_ad]
+
+@admin.register(models.Stock)
+class StockAdmin(admin.ModelAdmin):
+	actions = [dublicate_ad]
--- app/data/Index.py
+++ app/data/Index.py
@@ -26,6 +26,7 @@ data = {
         'about': {
             'title': 'о нас',
             'text_list': [
+                'Группа охранных предприятий "Бумеранг" прочно обосновалась на рынке предоставления охранных услуг, более чем двадцатилетнюю работу создало себе имидж надежного, высокоинтеллектуального охранного предприятия.',
                 'Руководящий состав группы охранных предприятий «Бумеранг» имеет большой опыт организации эффективной охраны предприятий различного профиля. На предприятии имеется резерв для оперативной замены и усиления постов охраны. Работа охранников на объектах контролируется круглосуточно. С 17-00 до 9-00 работает оперативный дежурный, которому каждый час докладывается оперативная обстановка на всех объектах. Также, в ночное время осуществляются проверки несения службы сотрудниками отдела внутреннего контроля.',
                 'Соучредителями ЧОО «МОП Центр» выступают охранные организации, расположенные в каждом субъекте Российской Федерации в Центральном, Приволжском, Северо-Западном, Южном и Северо-Кавказском федеральных округах, а также ряд охранных организаций в Дальне-Восточном и Сибирском федеральных округах; Объединение охранных организаций в рамках проекта «МОП Центр» направлено на координацию защиты интересов клиентов, работающих в нескольких субъектах Российской Федерации.'
             ],
@@ -77,13 +78,14 @@ data = {
         'services': {
             'title': 'services',
             'all_services': 'ALL SERVICES',
-            'desc': 'The group of security companies "Boomerang" for more than twenty years of existence has created an image of a reliable, highly intelligent security company.',
+            'desc': '',
             'read_more': 'Read more',
             'slides': []
         },
         'about': {
             'title': 'about us',
             'text_list': [
+                'The group of security companies "Boomerang" for more than twenty years of existence has created an image of a reliable, highly intelligent security company.',
                 'The management of the group of security companies "Boomerang" has extensive experience in organizing effective security for facilities of various profiles. The enterprise has a reserve for prompt replacement and strengthening of security posts. The work of security guards at the facilities is monitored around the clock. From 17-00 to 9-00 an operational duty officer works, to whom the operational situation at all objects is reported every hour. Also, at night, inspections of service by employees of the internal control department are carried out.',
                 'The co-founders of the MOP Center private limited liability company are security organizations located in each constituent entity of the Russian Federation in the Central, Volga, North-West, South and North-Caucasian Federal Districts, as well as a number of security organizations in the Far-Eastern and Siberian Federal Districts; The association of security organizations within the framework of the MOP Center project is aimed at coordinating the protection of the interests of clients working in several constituent entities of the Russian Federation.'
             ],
